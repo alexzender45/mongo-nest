89dd907556a357312d4207696afa92fd08ae6b2d
Abiokorigho Samuel <alex@Abiokorighos-MacBook-Pro.local>
2022-04-02 13:25:49 +0100

    Added

---
+++ src/breeds/breeds.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Breed, BreedDocument } from 'src/schemas/breed.schema';
+import { CreateBreedDto } from './dto/create-breed.dto';
+import { UpdateBreedDto } from './dto/update-breed.dto';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class BreedsService {
+  constructor(@InjectModel(Breed.name) private breedModel: Model<BreedDocument>) {}
+  create(createBreedDto: CreateBreedDto): Promise<Breed> {
+    return this.breedModel.create(createBreedDto);
+  }
+
+  findAll() {
+    return this.breedModel.find();
+  }
+
+  findOne(name: string) {
+    return this.breedModel.findOne({ name });
+  }
+
+  update(name: string, updateBreedDto: UpdateBreedDto) {
+    return this.breedModel.updateOne({ name }, updateBreedDto);
+  }
+
+  remove(name: string) {
+    return this.breedModel.deleteOne({ name });
+  }
+}
